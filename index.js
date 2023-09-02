c2c937bf5f325ab6409d3a4e5ee3982cddc0892e
JoaoGabriel <joaogabrieldev@gmail.com>
2023-09-02 01:20:42 -0300

    Second commit

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send(`Hello ${name}!`);
 });
 
+app.get('/jg', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`AAAAAAAAAAAAAAAAA ${name}!`);
+});
+
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
